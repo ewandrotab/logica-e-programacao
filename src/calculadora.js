1eaaace9961ec906835992a2e0cda2b787d8c2dc
Ewandro Luiz Taborda <ewandro.taborda@gmail.com>
2025-08-29 17:04:09 -0300

    Adicionada função para cálcular a média de dois números

--- src/calculadora.js
+++ src/calculadora.js
@@ -3,6 +3,18 @@ function somarDoisNumeros(valor1, valor2) {
     return resultado
 }
 
+function calcularMediaDeDoisNumeros(valor1, valor2) {
+
+    // 1. Soma todos os valoes
+    const resultadoSomaDeDoisValores = somarDoisNumeros(valor1, valor2)
+
+    // 2. Divide pela quantidade de números passados
+    const resultadoMediaDeDoisValores = resultadoSomaDeDoisValores / 2
+
+    // 3. Retornar o resultado
+    return resultadoMediaDeDoisValores
+}
+
 module.exports = {
     somarDoisNumeros
 }
